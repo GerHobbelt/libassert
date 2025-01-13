aa50353f700de135b4e787764d29a517900becc9
Ger Hobbelt <ger@hobbelt.com>
2025-01-14 00:06:25 +0100

    fix warning C5240: 'nodiscard': attribute is ignored in this syntactic position. See also: https://stackoverflow.com/questions/67502217/warning-c5240-nodiscard-attribute-is-ignored-in-this-syntactic-position

--- include/libassert/assert.hpp
+++ include/libassert/assert.hpp
@@ -484,7 +484,7 @@ namespace libassert::detail {
 
     // TODO: Re-evaluate benefit of this at all in non-cold path code
     template<typename A, typename B, typename C, typename... Args>
-    LIBASSERT_ATTR_COLD LIBASSERT_ATTR_NOINLINE [[nodiscard]]
+		[[nodiscard]] LIBASSERT_ATTR_COLD LIBASSERT_ATTR_NOINLINE
     expression_decomposer<A, B, C> process_assert_fail_m(
         expression_decomposer<A, B, C> decomposer,
         const assert_static_parameters* params,
