0511de4228fc50057e1cd5eefbb71a68ccb1fa00
Ger Hobbelt <ger@hobbelt.com>
2025-06-16 20:35:36 +0200

    fix bug in one of the macros: the assertion reporting code was invoked when the expression *passed* instead of when it *failed*! (my own mistake)

--- include/libassert/assert.hpp
+++ include/libassert/assert.hpp
@@ -880,7 +880,7 @@ LIBASSERT_EXPORT void libassert_breakpoint_if_debugger_present(void);
         LIBASSERT_WARNING_PRAGMA_PUSH_MSVC \
         LIBASSERT_IGNORE_UNUSED_VALUE \
         LIBASSERT_CHECK_EXPR_TYPE_AS_BOOLEAN(expr); \
-        if(!!(expr)) { \
+        if(!(expr)) { \
             LIBASSERT_BREAKPOINT_IF_DEBUGGING_ON_FAIL(); \
             failaction \
             libassert_detail_primitive_assert_impl( \
